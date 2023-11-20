b0fc304893bd13175f0aa1e6c3b0a6722f898f9c
joaosour <joaovictorsousa45@gmail.com>
2023-11-20 15:38:57 -0300

    atualização crud master

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import usersRoutes from "./routes/users.js"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/", usersRoutes)
+
+app.listen(8800)
+
